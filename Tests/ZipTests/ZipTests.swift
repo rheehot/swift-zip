23719830b2811192678708b77ea96396efa4c2fa
Jaehong Kang <sinoru@me.com>
2020-02-10 11:50:05 +0900

    Implement intial Zip type

--- Tests/ZipTests/ZipTests.swift
+++ Tests/ZipTests/ZipTests.swift
@@ -2,14 +2,14 @@ import XCTest
 @testable import Zip
 
 final class ZipTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Zip().text, "Hello, World!")
-    }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+//    func testExample() {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct
+//        // results.
+//        XCTAssertEqual(Zip().text, "Hello, World!")
+//    }
+//
+//    static var allTests = [
+//        ("testExample", testExample),
+//    ]
 }
