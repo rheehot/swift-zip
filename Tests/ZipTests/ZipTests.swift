66dbbf685937beddaa641ac78b9508d0e6346c80
Jaehong Kang <sinoru@me.com>
2020-02-10 12:48:15 +0900

    Implement testUnzip test case

--- Tests/ZipTests/ZipTests.swift
+++ Tests/ZipTests/ZipTests.swift
@@ -31,6 +31,40 @@ final class ZipTests: XCTestCase {
         }
     }
 
+    func testUnzip() throws {
+        let fileURL = URL(fileURLWithPath: #file)
+            .deletingLastPathComponent()
+            .appendingPathComponent("jikji.epub+zip.epub")
+
+        let unzipDestinationURL = URL(fileURLWithPath: NSTemporaryDirectory())
+            .appendingPathComponent(UUID().uuidString)
+
+        func test() {
+            do {
+                let zip = try Zip(contentsOf: fileURL)
+
+                try zip.unzip(to: unzipDestinationURL)
+            } catch {
+                XCTAssertThrowsError(error)
+            }
+        }
+
+        if #available(OSX 10.15, *) {
+            measure(metrics: [
+                XCTCPUMetric(),
+                XCTMemoryMetric(),
+                XCTClockMetric()
+            ], block: test)
+        } else {
+            measure(test)
+        }
+
+        let subpaths = try FileManager.default.subpathsOfDirectory(atPath: unzipDestinationURL.path)
+        XCTAssertEqual(subpaths.count, 104)
+
+        try FileManager.default.removeItem(at: unzipDestinationURL)
+    }
+
     static var allTests = [
         ("testGetItem", testGetItem),
     ]
