a061049530f562ab3cb6ceb4f5b3c13f6b22b190
Jaehong Kang <sinoru@me.com>
2020-02-10 12:29:08 +0900

    Implement testGetItem test case

--- Tests/ZipTests/ZipTests.swift
+++ Tests/ZipTests/ZipTests.swift
@@ -2,14 +2,36 @@ import XCTest
 @testable import Zip
 
 final class ZipTests: XCTestCase {
-//    func testExample() {
-//        // This is an example of a functional test case.
-//        // Use XCTAssert and related functions to verify your tests produce the correct
-//        // results.
-//        XCTAssertEqual(Zip().text, "Hello, World!")
-//    }
-//
-//    static var allTests = [
-//        ("testExample", testExample),
-//    ]
+    func testGetItem() throws {
+        let fileURL = URL(fileURLWithPath: #file)
+            .deletingLastPathComponent()
+            .appendingPathComponent("jikji.epub+zip.epub")
+
+        let test = {
+            do {
+                let zip = try Zip(contentsOf: fileURL)
+
+                let item = try zip.getItem(atPath: "META-INF/container.xml", caseSensitive: true)
+
+                XCTAssertNotNil(item)
+                XCTAssertEqual(item?.data.count, 259)
+            } catch {
+                XCTAssertThrowsError(error)
+            }
+        }
+
+        if #available(OSX 10.15, *) {
+            measure(metrics: [
+                XCTCPUMetric(),
+                XCTMemoryMetric(),
+                XCTClockMetric()
+            ], block: test)
+        } else {
+            measure(test)
+        }
+    }
+
+    static var allTests = [
+        ("testGetItem", testGetItem),
+    ]
 }
